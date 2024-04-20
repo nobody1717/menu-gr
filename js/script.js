e611814dec8b5d43efc53d34bccd1b1f0c952839
nobody <105120943+nobody1717@users.noreply.github.com>
2024-04-20 14:48:39 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+    el: "html",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x0,
+    shininess: 150.00,
+    waveHeight: 7.00,
+    waveSpeed: 2.00,
+    zoom: 1.29
+  })
